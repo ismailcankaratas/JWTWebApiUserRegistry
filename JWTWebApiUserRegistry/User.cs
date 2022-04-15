b370544a9dbd218b2fb7b38281d6d717a7bd1334
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2022-04-15 15:38:43 +0300

    Proje dosyası ekle.

---
+++ JWTWebApiUserRegistry/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JWTWebApiUserRegistry
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ JWTWebApiUserRegistry/UserRegister.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTWebApiUserRegistry
+{
+    public class UserRegister
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
